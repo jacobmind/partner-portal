3a9bcb956fbab9b13226fa11ac7f9e88ba3d312e
jasonmindcom <kubastanosz@gmail.com>
2025-04-18 00:11:14 +0700

    Add API endpoints and partner model for partner service integration

---
+++ src/app/models/partner.model.ts
@@ -0,0 +1,9 @@
+export interface Partner {
+  id: string;
+  partnerName: string;
+  partnerType: string;
+  contract: string;
+  grosssales: number;
+  commissions: number;
+  conversions: number;
+}
--- src/app/services/partner.service.ts
+++ src/app/services/partner.service.ts
@@ -1,26 +1,15 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-
-export interface Partner {
-  id: string;
-  partnerName: string;
-  partnerType: string;
-  contract: string;
-  grosssales: number;
-  commissions: number;
-  conversions: number;
-}
-
+import { Partner } from '../models/partner.model';
+import {PARTNER_API_URL} from '../constants/api-endpoints';
 @Injectable({
   providedIn: 'root',
 })
 export class PartnerService {
-  private apiUrl = 'https://mockanapi.com/s/67ae1b3403f9ffca6f47eb79/partners?mock_delay=1000';
-
   constructor(private http: HttpClient) {}
 
   getPartners(): Observable<Partner[]> {
-    return this.http.get<Partner[]>(this.apiUrl);
+    return this.http.get<Partner[]>(PARTNER_API_URL);
   }
 }
