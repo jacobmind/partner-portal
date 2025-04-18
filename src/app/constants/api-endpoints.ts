d7f3d7610853335aa8fee1d126e5d12860b96921
jasonmindcom <kubastanosz@gmail.com>
2025-04-18 22:22:43 +0700

    Update partner API URL mock delay and enhance loading state UI in dashboard

--- src/app/constants/api-endpoints.ts
+++ src/app/constants/api-endpoints.ts
@@ -1,2 +1,2 @@
 export const BASE_API_URL = 'https://mockanapi.com/s/67ae1b3403f9ffca6f47eb79';
-export const PARTNER_API_URL = `${BASE_API_URL}/partners?mock_delay=1000`;
+export const PARTNER_API_URL = `${BASE_API_URL}/partners?mock_delay=5000`;
